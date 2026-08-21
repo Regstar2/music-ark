eb6688594acdc4dd83417232f6050cd5eff5864c
Regstar2 <carkov195@gmail.com>
2026-08-21 12:26:12 +0300

    test(v0.14): cover stale Local Library async results

--- ui/musicark_ui/test/v014_performance_hardening_test.dart
+++ ui/musicark_ui/test/v014_performance_hardening_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -6,6 +8,30 @@ import 'package:musicark_ui/local_library_page.dart';
 import 'package:musicark_ui/main.dart' show FakeMusicArkBridge;
 import 'package:musicark_ui/musicark_bridge.dart';
 import 'package:musicark_ui/yandex_workspace.dart' as yandex;
+
+Map<String, dynamic> _localTrack(int id, String title) => {
+  'id': id,
+  'rootId': 1,
+  'path': 'C:/Music/$title.mp3',
+  'fileName': '$title.mp3',
+  'title': title,
+  'artists': const ['Artist'],
+  'album': 'Album',
+  'durationSeconds': 180.0,
+  'codec': 'mp3',
+};
+
+Map<String, dynamic> _localPage(
+  List<Map<String, dynamic>> items, {
+  int? count,
+  int limit = 250,
+  int offset = 0,
+}) => {
+  'count': count ?? items.length,
+  'limit': limit,
+  'offset': offset,
+  'items': items,
+};
 
 class _LargeLocalBridge extends FakeMusicArkBridge {
   _LargeLocalBridge() : super(startSignedIn: true);
@@ -49,6 +75,27 @@ class _LargeLocalBridge extends FakeMusicArkBridge {
   }
 }
 
+class _StaleLocalBridge extends FakeMusicArkBridge {
+  _StaleLocalBridge() : super(startSignedIn: true);
+
+  final Completer<Map<String, dynamic>> firstSearch = Completer();
+  final Completer<Map<String, dynamic>> secondSearch = Completer();
+
+  @override
+  Future<Map<String, dynamic>> localTracks({
+    int limit = 1000,
+    int offset = 0,
+    String search = '',
+    String sort = 'artist',
+    int? rootId,
+    List<int>? rootIds,
+  }) {
+    if (search == 'first') return firstSearch.future;
+    if (search == 'second') return secondSearch.future;
+    return Future.value(_localPage([_localTrack(1, 'Initial')], limit: limit));
+  }
+}
+
 void main() {
   Future<void> pumpLocal(WidgetTester tester, MusicArkBridgeClient bridge) async {
     await tester.binding.setSurfaceSize(const Size(1500, 900));
@@ -77,6 +124,33 @@ void main() {
     expect(bridge.requestedLimits.first, 250);
     expect(find.byKey(const Key('local-library-page')), findsOneWidget);
     expect(find.text('Track 10000'), findsNothing);
+  });
+
+  testWidgets('Local Library ignores a slower stale async search result', (
+    tester,
+  ) async {
+    final bridge = _StaleLocalBridge();
+    await bridge.localRootAdd(r'C:\Music');
+    await pumpLocal(tester, bridge);
+    expect(find.text('Initial'), findsOneWidget);
+
+    final search = find.byKey(const Key('local-search'));
+    await tester.enterText(search, 'first');
+    await tester.testTextInput.receiveAction(TextInputAction.done);
+    await tester.pump();
+
+    await tester.enterText(search, 'second');
+    await tester.testTextInput.receiveAction(TextInputAction.done);
+    await tester.pump();
+
+    bridge.secondSearch.complete(_localPage([_localTrack(2, 'Second Result')]));
+    await tester.pumpAndSettle();
+    expect(find.text('Second Result'), findsOneWidget);
+
+    bridge.firstSearch.complete(_localPage([_localTrack(3, 'First Stale Result')]));
+    await tester.pumpAndSettle();
+    expect(find.text('Second Result'), findsOneWidget);
+    expect(find.text('First Stale Result'), findsNothing);
   });
 
   testWidgets('Yandex track search is debounced and track list has fixed extent', (
